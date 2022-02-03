198f88a3ee96178abc0fb9c4a3c7d473d7f82e1e
Carlos Llanillo <carlos.llanilloantolin@iesmiguelherrero.com>
2022-02-03 16:18:35 +0100

    pepo clawn

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
 
-  nombre = "Carlos"
-  edad = 18
+  nombre = "Carlos";
+  edad = 18;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  usuarioRegistrado = true;
+
+  getResgistroUsuario() {
+    return this.usuarioRegistrado;
+  }
+
+  setResgistroUsuario() {
+    alert("Usuario registrado");
   }
 
 }
